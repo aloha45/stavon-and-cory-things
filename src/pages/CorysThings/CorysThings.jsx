e0c67ba9054aa1c100694cac8e9713b41ce9bd81
Cory Spicer <corys4139@gmail.com>
2020-09-11 11:23:52 -0400

    Cory things page and component

---
+++ src/pages/CorysThings/CorysThings.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CoryThing from '../../components/CoryThing/CoryThing'
+
+const CorysThings = (props) => {
+  return ( 
+    <>
+      <h1>Corys's Things</h1>
+      {props.bensThings.map((thing, idx) => 
+        <CoryThing 
+          key={idx}
+          thing={thing}
+        />
+      )}
+
+    </>
+   );
+}
+ 
+export default CorysThings;
